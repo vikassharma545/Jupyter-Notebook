9fedef7cdfc81df6e37b8957ebc2e343aca36148
Vikas Sharma <Jnv2252@gmail.com>
2025-06-25 13:51:32 +0530

    added admin request permission

--- setup_context_menu.bat
+++ setup_context_menu.bat
@@ -1,4 +1,26 @@
 @echo off
+:: Check for admin permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: Install dependencies
+echo Installing Python dependencies...
+pip install notebook==6.5.7
+
 echo Installing Jupyter context menu...
 reg import open-with-jupyter.reg
 if %ERRORLEVEL%==0 (
